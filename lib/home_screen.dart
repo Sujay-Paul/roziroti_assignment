6a3f47b720e5f9ab3991e710fd8d5c50684b709e
SUJAY PAUL <sujaypaul0912@gmail.com>
2023-11-14 13:12:05 +0530

    initial commit

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:roziroti_assignment/shopping_filters.dart';
+
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -40,55 +42,99 @@ class HomeScreen extends StatelessWidget {
           ),
         ],
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Column(
-          children: [
-            TextField(
-              decoration: InputDecoration(
-                hintText: 'Search...',
-                prefixIcon: const Icon(Icons.search),
-                suffixIcon: const Icon(Icons.filter_alt_sharp),
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(20),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(14),
+          child: Column(
+            children: [
+              TextField(
+                decoration: InputDecoration(
+                  hintText: 'Search...',
+                  prefixIcon: const Icon(Icons.search),
+                  suffixIcon: const Icon(Icons.filter_alt_sharp),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(20),
+                    borderSide: BorderSide.none,
+                  ),
+                  filled: true,
+                  fillColor: Colors.grey[200],
                 ),
-                filled: true,
-                fillColor: Colors.grey[300],
               ),
-            ),
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    const Text('Special Offers'),
-                    TextButton(
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      const Text('Special Offers',style: TextStyle(fontWeight: FontWeight.w800,fontSize: 18)),
+                      TextButton(
                         onPressed: () {},
                         child: const Text(
                           'See All',
                           style: TextStyle(color: Colors.black),
-                        ))
-                  ],
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Container(
-                    width: double.infinity,
-                    height: 200,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(20),
-                      image: const DecorationImage(
-                        image: NetworkImage(
-                            'https://c8.alamy.com/comp/2D7GAPB/online-sale-shopping-vector-banner-design-online-shopping-text-with-phone-cart-and-paper-bag-elements-in-smartphone-app-store-for-mobile-business-2D7GAPB.jpg'),
-                        fit: BoxFit.cover,
+                        ),
+                      )
+                    ],
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Container(
+                      width: double.infinity,
+                      height: 200,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(20),
+                        image: const DecorationImage(
+                          image: NetworkImage(
+                              'https://c8.alamy.com/comp/2D7GAPB/online-sale-shopping-vector-banner-design-online-shopping-text-with-phone-cart-and-paper-bag-elements-in-smartphone-app-store-for-mobile-business-2D7GAPB.jpg'),
+                          fit: BoxFit.cover,
+                        ),
                       ),
                     ),
                   ),
-                ),
-              ],
-            )
-          ],
+                  const SizedBox(height: 10),
+                  GridView.builder(
+                    physics: const NeverScrollableScrollPhysics(),
+                    shrinkWrap: true,
+                    gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 4,
+                      crossAxisSpacing: 10.0,
+                      mainAxisSpacing: 10.0,
+                    ),
+                    itemCount: shoppingFilters.length,
+                    itemBuilder: (context, index) {
+                      return InkWell(
+                        onTap: () {},
+                        child: Column(
+                          children: [
+                            Container(
+                              width: 60,
+                              height: 60,
+                              decoration: BoxDecoration(
+                                color: Colors.grey[300],
+                                borderRadius: BorderRadius.circular(100),
+                              ),
+                              child: Center(
+                                child: Icon(
+                                  shoppingFilters[index].icon,
+                                  size: 20,
+                                  color: Colors.black,
+                                ),
+                              ),
+                            ),
+                            const SizedBox(height: 8),
+                            Text(
+                              shoppingFilters[index].name,
+                              style: const TextStyle(fontSize: 12),
+                            ),
+                          ],
+                        ),
+                      );
+                    },
+                  ),
+                ],
+              )
+            ],
+          ),
         ),
       ),
     );
---
+++ lib/shopping_filters.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class ShoppingFiltersModel{
+  final String name;
+  final IconData icon;
+  
+  const ShoppingFiltersModel({required this.name, required this.icon});
+
+}
+
+List<ShoppingFiltersModel> shoppingFilters= [
+  const ShoppingFiltersModel(name: 'Appliances', icon: Icons.home),
+  const ShoppingFiltersModel(name: 'Shoes', icon: Icons.work),
+  const ShoppingFiltersModel(name: 'Appliances', icon: Icons.shopping_bag),
+  const ShoppingFiltersModel(name: 'Electronics', icon: Icons.tv),
+  const ShoppingFiltersModel(name: 'Watch', icon: Icons.watch),
+  const ShoppingFiltersModel(name: 'Mobile', icon: Icons.android),
+  const ShoppingFiltersModel(name: 'Kitchen', icon: Icons.kitchen),
+  const ShoppingFiltersModel(name: 'Toys', icon: Icons.toys),
+];
